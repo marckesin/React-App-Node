1b9dfbcd840c0edb480f7436a4915f0bd6006ff3
marckesin <192.168.2.1@protonmail.com>
2022-05-29 19:09:13 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=flat&logo=react&logoColor=%2361DAFB) ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=flat&logo=node.js&logoColor=white) ![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=flat&logo=express&logoColor=%2361DAFB)
+
 # React App com NodeJS
 
-React app utilizando Node/Express como back-end.
+React app utilizando Node/Express como backend.
 
 Fonte: [https://www.freecodecamp.org/news/how-to-create-a-react-app-with-a-node-backend-the-complete-guide/](https://www.freecodecamp.org/news/how-to-create-a-react-app-with-a-node-backend-the-complete-guide/)
--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const logger = require('morgan');
-const path = require('path');
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
 
 const port = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('/api', (req, res) => {
-    res.json({ message: "React App e NodeJS." });
+app.get("/api", (req, res) => {
+  res.json({ message: "React App e NodeJS." });
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
